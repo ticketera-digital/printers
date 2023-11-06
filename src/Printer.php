26ded0b5ae0aa23b7b1801518cb625ed9b59cc63
Carlos Martínez <cmtinz@gmail.com>
2023-11-06 12:00:29 -0300

    Send print job

--- src/Printer.php
+++ src/Printer.php
@@ -2,10 +2,30 @@
 
 namespace Ticketeradigital\Printers;
 
-class Printer
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class Printer extends Model
 {
-    public static function test(): string
+
+    public $fillable = ["ip", "mac", "name"];
+
+    public function sendJob(array $job): void
     {
-        return 'Hola';
+        $payload = [
+            "printerId" => $this->id,
+            "job" => $job
+        ];
+        try {
+            Http::post(config("printers.proxy_url") . "/print-job", $payload)->throw();
+        } catch (RequestException $e) {
+            throw new PrintException($e->response->json()['error']);
+        } catch (ConnectionException $e) {
+            throw new PrintException("Error al conectar con el proxy de impresión");
+        }
     }
+
 }
